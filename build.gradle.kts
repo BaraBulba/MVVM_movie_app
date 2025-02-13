51eb2c9bbacd70455ca64850b44a2da18a053311
BaraBulba <andrewsolokhov@gmail.com>
2025-02-13 12:57:13 +0200

    updating plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,17 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
-        val navVersion = "2.7.6"
+        classpath("com.google.gms:google-services:4.4.2")
+        val navVersion = "2.8.7"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.3")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
 }
